51875f4ba9459855bf961b31aee6e49b978e4036
developershailendra <85412261+developershailendra@users.noreply.github.com>
2021-11-23 20:38:52 +0530

    Update moneyhistory.php

--- moneyhistory.php
+++ moneyhistory.php
@@ -1,17 +1,16 @@
-<center><a href="https://localhost/viewallcustomer.php/">back to view all customer</a></center>
+<center><a href="viewallcustomer.php">back to view all customer</a></center>
 <?php
 $name = $_GET['Id'];
 $person1 = $_POST['person2'];
 $Amount = $_POST['amount'];
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bank";
+$username = "id16815079_root";
+$password = "c|%CDDFGuk)Ap5ob";
+$dbname = "id16815079_bank";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 $sql = "SELECT * FROM `bank_table` WHERE `Customer_id`=".$name;
 $result = mysqli_query($conn,$sql);
 $resultcheck = mysqli_num_rows($result);
@@ -22,11 +21,6 @@ while($row =  mysqli_fetch_assoc($result)){
              die();
             }
     }
-
-
-
-
-
 $sql = "INSERT INTO transaction_history (To_customer, From_customer, Amount) VALUES (".$name."," .$person1.",".$Amount.")";
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
@@ -34,11 +28,7 @@ if (mysqli_query($conn, $sql)) {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
  }
 $conn->close();
-
 ?>
-
-
-
 <?php
    if(isset($_POST['Submit']))
    {
@@ -46,38 +36,23 @@ $conn->close();
       $person2=(int)$_POST['person2'];
       $amount=(int)$_POST['amount'];
       $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $dbname = "bank";
+$username = "id16815079_root";
+$password = "c|%CDDFGuk)Ap5ob";
+$dbname = "id16815079_bank";
     $con = new mysqli($servername, $username, $password, $dbname);
     if ($con->connect_error) {
       die("Connection failed: " . $con->connect_error);
     }
-    // $balence_check_query = "SELECT *  FROM bank_table WHERE Customer_id = ".$person1;
-    // $result_2 = $con->query($balence_check_query);
-    // while($row = mysqli_fetch_assoc($result_2)){
-    //   $Current_balance = $row['Current_Balence'];
-
-    // }
-    // if($amount < $Current_balance || $amount <=0){
-    //   die("Connection failed: due to amount " . $conn->connect_error);
-    // }
-   
-
-    
-
-            $sql = "UPDATE bank_table SET Current_Balance = Current_Balance +".$amount." WHERE Customer_id = ".$person2;
-    $sql1 = "UPDATE bank_table SET Current_Balance = Current_Balance -".$amount." WHERE Customer_id = ".$person1;
+    $sql = "UPDATE bank_table SET Current_balance = Current_balance +".$amount." WHERE Customer_id = ".$person2;
+    $sql1 = "UPDATE bank_table SET Current_balance = Current_balance -".$amount." WHERE Customer_id = ".$person1;
     if (($con->query($sql) === TRUE)&&($con->query($sql1) === TRUE)) {
       echo  "";
-
     } else {
       echo "Error updating record: " . $con->error;
     }
 
     $con->close();
-   
-  }
+   }
 ?>
 
 
